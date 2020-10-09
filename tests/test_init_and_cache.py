b9e31bcbcc8bda9a7b804e1cc51626ec42790844
Alexey <alexeybe@checkpoint.com>
2020-10-09 13:03:00 +0300

    fix path

--- tests/test_init_and_cache.py
+++ tests/test_init_and_cache.py
@@ -4,8 +4,8 @@ import pdb
 
 sys.path.insert(0, os.path.abspath("../src"))
 sys.path.insert(0, "/Users/alexeybe/private/IP/ip")
-sys.path.insert(0, "/Users/alexeybe/private/aws_api/ignore")
-sys.path.insert(0, "/Users/alexeybe/private/aws_api/src/base_entities")
+sys.path.insert(0, "~/private/aws_api/ignore")
+sys.path.insert(0, "~/private/aws_api/src/base_entities")
 
 from aws_api import AWSAPI
 import ignore_me
